712e6889f65660e07d0e90627921af936e7cdaec
Acaroji <ashleycaro93@gmail.com>
2019-09-02 22:01:21 +0200

    Updates

---
+++ precache-manifest.8b5aaa4af9fe14eda050480ae05898d1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14ea7e71c728a9585181c3131a6edf61",
+    "url": "/joyeux-anniversaire-mila/index.html"
+  },
+  {
+    "revision": "10521cc83511764cbe79",
+    "url": "/joyeux-anniversaire-mila/static/css/main.4604a183.chunk.css"
+  },
+  {
+    "revision": "d6f1e83cb6c050fc5581",
+    "url": "/joyeux-anniversaire-mila/static/js/2.d2793abb.chunk.js"
+  },
+  {
+    "revision": "10521cc83511764cbe79",
+    "url": "/joyeux-anniversaire-mila/static/js/main.90c44665.chunk.js"
+  },
+  {
+    "revision": "520c63b146f16c3fc890",
+    "url": "/joyeux-anniversaire-mila/static/js/runtime~main.e1c8a222.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/joyeux-anniversaire-mila/static/media/logo.5d5d9eef.svg"
+  }
+]);
