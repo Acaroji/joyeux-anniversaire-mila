eb96e4fb2d7a1da7b660cfdc1ec1c0d86b2ce53b
Acaroji <ashleycaro93@gmail.com>
2019-09-02 22:40:32 +0200

    adding website content

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
+    
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1"></meta>
+      <div className="landingPage">
+        <div className="topLandingPAge">
+          <div className="nombreMila">MILA</div>
+          <div className="pensamientosMila">60 pensées pour Mila</div>
+        </div>
+        <div className="landingBody">
+          <div className="miniNavbar">
+            <ul className="navBar"></ul>
+          </div>
+          <img src="./images/MilaNinos2.jpg" height="250px" weigth="250px" alt="family"/>
+          <div className="information">
+             Samedi 5 Octobre 2019, 
+             Nous surprendrons Mila pour ses 60 ans à Auvers sur Oise
+          </div>
+          <img src="./images/dry-leaf1.png" alt="hoja" />
+          <div className="messageBottom"> </div>
+          <div className="confirmation">
+            <a href="https://forms.gle/NfxJTqnuWzhw8z3N6"> <button className="button"> Confirmer ma participation</button></a> 
+          </div> 
+        </div>
+      </div>
     </div>
   );
 }
